affe0e69bb1850ed959eae4783fd6ff72d7a52b4
svvmy <benidir91@hotmail.fr>
2022-03-22 16:03:55 +0100

    test méthode de réservation

--- model/main.java
+++ model/main.java
@@ -3,5 +3,12 @@ package model;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World ca v");
+
+       
+        Compte compte = new Compte(1,"samy",545,060);
+
+       int numR = compte.PrendreUneReservation("01/10/2000", "normal", "samy");
+       System.out.println("voici cotre numero de reservation"+ " " +numR);
+
     }
 }
