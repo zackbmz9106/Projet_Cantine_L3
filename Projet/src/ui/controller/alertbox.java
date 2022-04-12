4d7653d0a0577e246a62b74d17448205b31f229c
zackbmz9106 <zakariunibet@gmail.com>
2022-04-13 01:02:14 +0200

    Add alert box ( popUp)

---
+++ Projet/src/ui/controller/alertbox.java
@@ -0,0 +1,35 @@
+package ui.controller;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class alertbox {
+
+    public static void afficher(String title, String message)
+ { 
+     Stage window = new Stage();
+
+     window.initModality(Modality.APPLICATION_MODAL);
+     
+     window.setWidth(250);
+     window.setHeight(250);
+
+     Label label = new Label(); 
+     label.setText(message);
+     Button fermer = new Button ("OK"); 
+     fermer.setOnAction(e -> window.close()); 
+
+     VBox layout = new VBox(10); 
+     layout.getChildren().addAll(label, fermer);
+     layout.setAlignment(Pos.CENTER);
+
+     Scene scene = new Scene(layout);
+     window.setScene(scene);
+     window.showAndWait();
+ }    
+}
